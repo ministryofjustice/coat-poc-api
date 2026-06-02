9cf89fc04d7c9dee195e13706927c76ef289b6b1
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-06-02 17:24:25 +0100

    amend column name and variable name

--- app/models/cloudCostModel.js
+++ app/models/cloudCostModel.js
@@ -1,6 +1,6 @@
 const AthenaService = require('../services/AthenaService');
 
-async function fetchCloudCostData(billing_period, account_name) {
+async function fetchCloudCostData(billing_period, line_item_usage_account_name) {
   const athena_client = new AthenaService(
     "cur_v2_database",
     process.env.APP_ENV
@@ -9,17 +9,17 @@ async function fetchCloudCostData(billing_period, account_name) {
   const query = `
     SELECT
       billing_period,
-      account_name,
+      line_item_usage_account_name,
       line_item_product_code,
       SUM(CAST(line_item_unblended_cost AS DOUBLE)) AS total_cost
     FROM data
     WHERE billing_period = '${billing_period}'
-      AND account_name = '${account_name}'
-    GROUP BY billing_period, account_name, line_item_product_code
+      AND line_item_usage_account_name = '${line_item_usage_account_name}'
+    GROUP BY billing_period, line_item_usage_account_name, line_item_product_code
     ORDER BY total_cost DESC;
   `;
 
-  const results = await athena.runQuery(query);
+  const results = await athena_client.runQuery(query);
   return results;
 }
 
