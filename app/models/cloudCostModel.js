78056fb7c2e8fe694b923d3b1de7222d2d4cece3
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2026-06-04 11:14:40 +0100

    template query with optional categorical variables from user input

--- app/models/cloudCostModel.js
+++ app/models/cloudCostModel.js
@@ -28,12 +28,43 @@ async function fetchCloudCostDaily(
 
   const query = `
     SELECT
+      ${account_name ? "account_name," : ""}
+      ${region ? "product_region_code," : ""}
+      ${environment ? "environment," : ""}
+      ${business_unit ? "business_unit," : ""}
+      ${application ? "tag_application," : ""}
+      ${namespace ? "tag_namespace," : ""}
+      ${service_area ? "tag_service_area," : ""}
+      ${owner ? "tag_owner," : ""}
+      ${product_name ? "product_name," : ""}
+
       usage_date,
+
       SUM(daily_cost) AS total_daily_cost
     FROM fct_daily_cost
     WHERE 
       usage_date BETWEEN DATE '${start_usage_date}' AND DATE '${end_usage_date}'
-    GROUP BY usage_date 
+      ${account_name ? `AND account_name = '${account_name}'` : ""}
+      ${region ? `AND product_region_code = '${region}'` : ""}
+      ${environment ? `AND environment = '${environment}'` : ""}
+      ${business_unit ? `AND business_unit = '${business_unit}'` : ""}
+      ${application ? `AND tag_application = '${application}'` : ""}
+      ${namespace ? `AND tag_namespace = '${namespace}'` : ""}
+      ${service_area ? `AND tag_service_area = '${service_area}'` : ""}
+      ${owner ? `AND tag_owner = '${owner}'` : ""}
+      ${product_name ? `AND product_name = '${product_name}'` : ""}
+    GROUP BY 
+      ${account_name ? "account_name," : ""}
+      ${region ? "product_region_code," : ""}
+      ${environment ? "environment," : ""}
+      ${business_unit ? "business_unit," : ""}
+      ${application ? "tag_application," : ""}
+      ${namespace ? "tag_namespace," : ""}
+      ${service_area ? "tag_service_area," : ""}
+      ${owner ? "tag_owner," : ""}
+      ${product_name ? "product_name," : ""}
+
+      usage_date
     ORDER BY usage_date;
   `;
 
